3d96301a85ec3fe859e74dda913fbf0149091612
minkee009 <minkee5004@gmail.com>
2025-08-08 21:27:39 +0900

    Update CrosshairController.cpp

--- GOTO_Game/src/component/CrosshairController.cpp
+++ GOTO_Game/src/component/CrosshairController.cpp
@@ -7,7 +7,8 @@ void GOTOEngine::CrosshairController::ChangeType(CrosshairType type)
 
 void GOTOEngine::CrosshairController::OnSceneLoaded()
 {
-
+	//설정 리셋
+	//일반 모드로 전환 -> 
 }
 
 void GOTOEngine::CrosshairController::Awake()
