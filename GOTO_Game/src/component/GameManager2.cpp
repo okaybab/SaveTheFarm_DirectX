ccf6967cfa53165eb21b741c29753912a4523538
ho991127 <ho991127@naver.com>
2025-08-12 17:38:41 +0900

    협동모드 시작 사격모드 조정

--- GOTO_Game/src/component/GameManager2.cpp
+++ GOTO_Game/src/component/GameManager2.cpp
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <Texture2D.h>
 #include "DefenseModeCameraManager.h"
+#include "CrosshairController.h"
 
 using namespace GOTOEngine;
 GameManager2* GameManager2::instance = nullptr;
@@ -243,6 +244,11 @@ void GameManager2::Awake() {
 		crop22->IncreaseRefCount();
 		crop21 = Resource::Load<Sprite>(L"../Resources/artResource/Sprint/Vegetable2/Vegetable2_1.png");
 		crop21->IncreaseRefCount();
+
+		auto p1 = GameObject::Find(L"Player1");
+		auto p2 = GameObject::Find(L"Player2");
+		p1->GetComponent<CrosshairController>()->ChangeType(CrosshairType::HoldingGun);
+		p2->GetComponent<CrosshairController>()->ChangeType(CrosshairType::HoldingGun);
 
 		auto timeitem = new GameObject;
 		timeitem->GetTransform()->SetParent(canvas->GetTransform());
